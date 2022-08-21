8d9a293246b6cc3a1e458c9a0d7e7e11343365ef
Ahmed Amin Hassan <ahmedAminHassanIsmail@gmail.com>
2022-08-21 20:37:47 +0200

    first commit

---
+++ MVVM_Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM_Demo
+//
+//  Created by Ahmed Amin on 21/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
